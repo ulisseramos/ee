10d82cf7459d22f9990e44639c8e61299e873031
Ulisses Ramos <ulissesramosp@gmail.com>
2025-07-01 18:05:32 -0300

    Orion

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -10,8 +10,8 @@ const Sheet = SheetPrimitive.Root
 const SheetTrigger = SheetPrimitive.Trigger
 const SheetClose = SheetPrimitive.Close
 
-const SheetPortal = ({ className, ...props }: SheetPrimitive.DialogPortalProps) => (
-  <SheetPrimitive.Portal className={cn(className)} {...props} />
+const SheetPortal = (props: SheetPrimitive.DialogPortalProps) => (
+  <SheetPrimitive.Portal {...props} />
 )
 SheetPortal.displayName = SheetPrimitive.Portal.displayName
 
